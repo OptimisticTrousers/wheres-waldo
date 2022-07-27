773ff72098925462dc486bf2da342988eac36d77
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-27 19:26:42 -0400

    Creating footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,14 @@
 import { StyledFooter } from "./styled/Footer.styled";
 import { Container } from "./styled/Container.styled";
+import {FaGithub} from 'react-icons/fa'
 export default function Footer() {
   return (
     <StyledFooter>
       <Container>
-        <h1>Stuff</h1>
+        <p>Copyright © 2022 Tony Isern</p>
+        <a href="https://github.com/OptimisticTrousers">
+          <FaGithub />
+        </a>
       </Container>
     </StyledFooter>
   );
--- src/components/styled/Footer.styled.js
+++ src/components/styled/Footer.styled.js
@@ -1,7 +1,29 @@
 import styled from "styled-components";
 
 export const StyledFooter = styled.footer`
-  background-color: blue;
-  height: 8%;
+  background-color: rgb(56, 56, 56);
+  padding: 1rem 0rem;
+  color: white;
+  div {
+    display: flex;
+    margin: 0 auto;
+    align-items: center;
+    gap: 0.5rem;
+    justify-content: center;
+  }
+  a {
+    border: 1px solid #fff;
+    border-radius: 50%;
+    color: #fff;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 10px;
+    height: 32px;
+    width: 32px;
+    text-decoration: none;
+  }
+  a:hover {
+    background-color: #22333B;
+  }
 `;
-
