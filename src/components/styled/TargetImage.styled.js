7b661f7a2750a5260483c26b40a7a651ab4f9e62
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-29 08:33:23 -0400

    Making menu more visible

--- src/components/styled/TargetImage.styled.js
+++ src/components/styled/TargetImage.styled.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 export const TargetImage = styled.div`
-width: 100px;
-height: 100px;
-background: none !important;
-border: none;
+  width: 100px;
+  height: 100px;
+  background: none !important;
+  border: none;
   border: 5px solid red;
-`
+`;
--- src/components/styled/TargetMenu.styled.js
+++ src/components/styled/TargetMenu.styled.js
@@ -2,4 +2,6 @@ import styled from "styled-components";
 
 export const TargetMenu = styled.ul`
   padding: 1rem;
+  color: white;
+  background-color: black;
 `;
