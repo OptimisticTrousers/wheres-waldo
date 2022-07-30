9784bb175cbb228d4671a956b598b3f9282ccdf4
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-30 15:16:15 -0400

    Removing the initialize of the webapp in this firebase-config.js file

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,12 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyBnErfDVMvYg8KdOujhCoYzmbfegZI1Z-I",
   authDomain: "wheres-waldo-53320.firebaseapp.com",
   projectId: "wheres-waldo-53320",
@@ -16,6 +16,3 @@ const firebaseConfig = {
   measurementId: "G-M3JV1QC326"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
