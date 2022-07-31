fea5f12edc184c04c011b305b2ee0cdb01b5c092
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-31 08:51:05 -0400

    Removing more unneeded comments

--- src/components/styled/Game.styled.js
+++ src/components/styled/Game.styled.js
@@ -6,13 +6,6 @@ export const StyledGame = styled.main`
   justify-content: center;
   flex: 1;
   padding: 3rem;
-  //background: linear-gradient(150deg,#ddd 49.63%,#6e88a1 100%);
-
-  /* div {
-    display: flex;
-    background-color: inherit;
-    justify-content:center;
-  } */
   div {
 
     position: relative;
@@ -21,12 +14,6 @@ export const StyledGame = styled.main`
     background-repeat: no-repeat;
     border-radius: 1.75%/2.3275%
   }
-  /* img {
-
-    margin-bottom: 3rem;
-    width: min-content;
-    height: min-content;
-  } */
 
   h1 {
     position: absolute;
