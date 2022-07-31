a3c9156556891f3b86bbc5b51bfade7f7edbc8da
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-31 18:33:49 -0400

    Creating a Control component

---
+++ src/components/styled/Controls.styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+export const StyledControls = styled.div`
+  display: flex;
+  position: relative;
+  width: 630px;
+  height: 70px;
+  box-sizing: border-box;
+  padding: 14px 20px 15px 20px;
+  margin-left: 44px;
+  margin-top: 15px;
+  border-radius: 30px;
+  z-index: 6;
+  backdrop-filter: blur(8px);
+  justify-content: space-between;
+`
