1f9136a2f579bf4923ef6d1e8a850b42d44bcfa7
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-02 10:58:00 -0400

    Writing test for Header.styled.test.js

--- src/__tests__/Header.styled.test.js
+++ src/__tests__/Header.styled.test.js
@@ -4,5 +4,22 @@ import "@testing-library/jest-dom";
 import StyledControls from "../components/styled/Controls.styled";
 import Game from "../components/Game";
 import userEvent from "@testing-library/user-event";
-import beach from '../assets/beach.jpg'
-import fruitland from '../assets/fruitland.jpg'
+import beach from "../assets/beach.jpg";
+import fruitland from "../assets/fruitland.jpg";
+import Header from "../components/styled/Header.styled";
+import { within } from "@testing-library/dom";
+
+describe("Header", () => {
+  test("user changing from light mode to dark mode", async () => {
+    render(<Header />);
+
+    const user = userEvent.setup();
+    const banner = screen.getByRole("banner");
+
+    within(banner).getByRole("button");
+
+    await user.click(banner);
+
+    expect(banner.backgroundColor).toEqual("black")
+  });
+});
