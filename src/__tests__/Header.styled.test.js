c18b7ca8e2c4a3ec2e7ca3925d549ad17e4dd512
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-02 11:37:27 -0400

    Writing tests for Header

--- src/__tests__/Header.styled.test.js
+++ src/__tests__/Header.styled.test.js
@@ -1,25 +1,30 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, waitForDomChange, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import StyledControls from "../components/styled/Controls.styled";
 import Game from "../components/Game";
 import userEvent from "@testing-library/user-event";
 import beach from "../assets/beach.jpg";
 import fruitland from "../assets/fruitland.jpg";
-import Header from "../components/styled/Header.styled";
-import { within } from "@testing-library/dom";
+import { StyledHeader } from "../components/styled/Header.styled";
+import { waitFor, within } from "@testing-library/dom";
 
 describe("Header", () => {
   test("user changing from light mode to dark mode", async () => {
-    render(<Header />);
+    render(<StyledHeader/>);
 
     const user = userEvent.setup();
-    const banner = screen.getByRole("banner");
 
-    within(banner).getByRole("button");
+    const banner = screen.queryByRole("banner")
+    const moon = screen.queryByRole("button");
 
-    await user.click(banner);
+    await user.click(moon);
 
-    expect(banner.backgroundColor).toEqual("black")
+    await waitFor(() => {
+
+      expect(banner.backgroundColor).toEqual("black");
+    })
+
+
   });
 });
