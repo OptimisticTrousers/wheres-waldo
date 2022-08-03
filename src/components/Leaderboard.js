fc561cb1dfa372d61426265c506bdd2a4fcaeca7
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-03 16:12:14 -0400

    Styling leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -3,19 +3,40 @@ import { StyledLeaderBoard } from "./styled/StyledLeaderBoard";
 export default function Leaderboard() {
   return (
     <StyledLeaderBoard>
-      <table>
-        <caption>Leaderboard</caption>
-        <thead>
-          <th>Position</th>
-          <th>Name</th>
-          <th>Time</th>
-        </thead>
-        <tbody>
-          <td>#1</td>
-          <td>Leandro Rezende</td>
-          <td>12</td>
-        </tbody>
-      </table>
+      <div>
+        <table>
+          <caption>Leaderboard</caption>
+          <thead>
+            <tr>
+              <th>Position</th>
+              <th>Name</th>
+              <th>Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>#1</td>
+              <td>Leandro Rezende</td>
+              <td>12</td>
+            </tr>
+            <tr>
+              <td>#1</td>
+              <td>Leandro Rezende</td>
+              <td>12</td>
+            </tr>
+            <tr>
+              <td>#1</td>
+              <td>Leandro Rezende</td>
+              <td>12</td>
+            </tr>
+            <tr>
+              <td>#1</td>
+              <td>Leandro Rezende</td>
+              <td>12</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </StyledLeaderBoard>
   );
 }
--- src/components/styled/StyledLeaderBoard.js
+++ src/components/styled/StyledLeaderBoard.js
@@ -1,8 +1,56 @@
 import styled from "styled-components";
 
 export const StyledLeaderBoard = styled.main`
-  background-color: #f5f7f8;
+  height: 100%;
+  width: 90%;
+
+  div {
+    background-color: white;
+    border-radius: 5px;
+    margin: 1rem;
+  }
+
+  th {
+  }
+
+  caption {
+    width: 100%;
+    text-align: start;
+    padding: 1rem;
+    border-bottom: 1px solid #f8f9f9;
+  }
+  th {
+    padding: 1rem;
+  }
   table {
-    padding: 2rem;
+    width: 100%;
+  }
+  td {
+    padding: 1rem;
+  }
+  tr {
+    margin: 0;
+  }
+  tbody {
+    width: 100%;
+    text-align: center;
+  }
+  tr:nth-child(even) td {
+    background-color: rgb(250, 250, 250);
+  }
+
+  tr:nth-child(odd) td {
+    background-color: white;
+  }
+
+  tr {
+    border: 10px solid red;
+  }
+
+  tr:hover > td:nth-child(1) {
+    border-left: 6px solid #69b2e1;
+  }
+  tr:hover {
+    cursor: pointer;
   }
 `;
