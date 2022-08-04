63c932c89f20bffa09ebb3501371ed3fe8462c80
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-04 11:23:29 -0400

    Cleaning up code

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -8,22 +8,19 @@ import track from "../assets/track.jpg";
 import winter from "../assets/winter.jpg";
 import { useContext, useState } from "react";
 import { ImageContext } from "../context/Store";
+import uniqid from 'uniqid'
 
 export default function Leaderboard() {
   const { images } = useContext(ImageContext);
   const [userClickedImage, setUserClickedImage] = useState(false);
 
-  function handleClick(event) {
+  function handleClick() {
     setUserClickedImage((prevValue) => !prevValue);
   }
 
-  const style = {
-    backgroundColor: userClickedImage && "rgb(254, 226, 226, 1)",
-  };
-
   const renderedImages = images.map(({ image, name }, index) => {
     return (
-      <div onClick={handleClick}>
+      <div onClick={handleClick} key={uniqid()}>
         <img src={image} alt={name} />
         <p>Level {index + 1}</p>
       </div>
