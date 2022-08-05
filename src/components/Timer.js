5bafdf65d1465c13e092e5c423c37fa1499cca33
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-05 19:11:30 -0400

    Cleaning up Timer.js

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,28 +1,24 @@
-import React, {Fragment, useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { formatTime } from "../utils";
 
 import { ImageContext } from "../context/Store";
 export function Timer() {
   const [timer, setTimer] = useState(0);
 
-  const {userWon} = useContext(ImageContext)
+  const { userWon } = useContext(ImageContext);
 
   useEffect(() => {
     let interval;
-    if(!userWon) {
+    if (!userWon) {
       interval = setInterval(() => {
-        setTimer((timer) => timer + 1)
-      }, 1000)
-    } 
-    // else if(userWon) {
-    //   clearInterval(interval)
-    //   setTimer(0)
-   
- //
-    return () => {
-      clearInterval(interval)
+        setTimer((timer) => timer + 1);
+      }, 1000);
     }
-  }, [userWon])
 
-  return <>{formatTime(timer)}</>
+    return () => {
+      clearInterval(interval);
+    };
+  }, [userWon]);
+
+  return <>{formatTime(timer)}</>;
 }
