a687025a8de55c1555b22c443fe94489d55b4e5e
OptimisticTrousers <tony.isern1@gmail.com>
2023-10-24 18:52:59 -0400

    Removing border-radius from image container

--- src/components/styled/ImageContainer.styled.js
+++ src/components/styled/ImageContainer.styled.js
@@ -5,7 +5,6 @@ export const ImageContainer = styled.div`
   background-image: url(${({ image }) => image});
   background-size: cover;
   background-repeat: no-repeat;
-  border-radius: 1.75%/2.3275%;
   height: 1280px;
   width: 1920px;
 
